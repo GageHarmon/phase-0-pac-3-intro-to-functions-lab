a1b455966bad97499bcd139a95ead4eccfe85636
Gage Harmon <gaharmon1@gmail.com>
2023-01-03 17:27:47 -0700

    commit tree 903b1b18349e0b3bf12ad980c4155d49638e1396

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  } 
+  function logShout(string) {
+    console.log('HELLO');
+  }
+  function logWhisper(string) {
+    console.log('hello');
+  }
+  function sayHiToHeadphonedRoommate(string1, string2, string3) {
+    const string1 = ("I can't hear you!")
+    const string2 = ('YES INDEED!')
+    const string3 = ('I would love to!')
+    if sayHiToHeadphonedRoommate = string1.toLowerCase
+    return string("I can't hear you!")
+    if sayHiToHeadphonedRoommate = string2.toUpperCase
+    return string('YES INDEED!')
+    if sayHiToHeadphonedRoommate = string3
+    return string('I would love to!')
+  }
+
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -52,3 +52,4 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
     );
   });
 })
+"mocha --timeout 5000 -R mocha-multi --reporter-options spec=-,json=.results.json --bail"
